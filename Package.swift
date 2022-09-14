6172a88d7f6821d935ef215457790e65389037db
Justin Schröder <justin.schroeder@calimoto.com>
2022-09-14 15:40:21 +0200

    adjust spm file

--- Package.swift
+++ Package.swift
@@ -20,7 +20,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "J2objcPackage",
-            dependencies: []),
+            dependencies: [],
+            cSettings: [.headerSearchPath("JRE.xcframework/Headers"), .headerSearchPath("JSON.xcframework/Headers"), .headerSearchPath("ProtobufRuntime.xcframework/Headers")]),
         .binaryTarget(name: "JRE", url: "https://u247826.your-storagebox.de/ios/dependencies/j2objc/2.8a/JRE.xcframework.zip", checksum: "5c4cad8f15332dce41199291530b1fee637505fbf61b8cf1f60c4edddfbf118e"),
         .binaryTarget(name: "JSON", url: "https://u247826.your-storagebox.de/ios/dependencies/j2objc/2.8a/JSON.xcframework.zip", checksum: "4c005b5e922d2d5a116060ddd207e522da2202c48388abddf8c82b27cb90f179"),
         .binaryTarget(name: "ProtobufRuntime", url: "https://u247826.your-storagebox.de/ios/dependencies/j2objc/2.8a/ProtobufRuntime.xcframework.zip", checksum: "4d4ae8387c6746e71359abf497c97f0e9c7f5d540eda4ec4ebcaeee6788879e6"),
